916e41050c5d1e68be66b42bb24dfc1c8d5348cb
Steven John Miller <steven.jmiller@gmail.com>
2015-04-20 18:53:12 +1000

    Change up

--- libs/accessSniff.js
+++ libs/accessSniff.js
@@ -1,27 +1,52 @@
 //
-// Grunt AccessSniff Task
+// Gulp AccessSniff Task
 // ------------------------
 
-module.exports = function(grunt) {
-  
-  var accessSniff = require('access-sniff');
+// through2 is a thin wrapper around node transform streams
+var gutil = require('gulp-util');
+var through = require('through2');
+var accessSniff = require('access-sniff');
 
-  grunt.registerMultiTask('accessibility', 'Use HTML codesniffer to grade accessibility', function() {
+var PluginError = gutil.PluginError;
 
-    var done = this.async();
-    var options = this.options({
-      ignore: [],
-      verbose: true,
-      force: false,
-      domElement: true,
-      reportType: null,
-      reportLocation : 'reports',
-      accessibilityrc: false,
-      accessibilityLevel: 'WCAG2A',
-    });
+// Consts
+const PLUGIN_NAME = 'gulp-accessibility';
 
-    accessSniff.start(this.filesSrc, options, done);
+// Plugin level function(dealing with files)
+function gulpAccessibility(options) {
+
+  var gulpOptions = options ? options : {
+    ignore: [],
+    verbose: true,
+    force: false,
+    domElement: true,
+    reportType: null,
+    reportLocation : 'reports',
+    accessibilityrc: false,
+    accessibilityLevel: 'WCAG2A',
+  };
+
+  var emailBuilder = new EmailBuilderCore(options);
+
+  // Creating a stream through which each file will pass
+  return through.obj(function(file, enc, cb) {
+
+    if (file.isNull()) {
+      // return empty file
+      cb(null, file);
+    }
+
+    if (file.isStream()) {
+      throw new PluginError(PLUGIN_NAME, 'Cannot read strams');
+    }
+
+    if (file.isBuffer()) {
+      accessSniff.start(file.path, options, cb);
+    }
 
   });
 
-};
+}
+
+// Exporting the plugin main function
+module.exports = gulpAccessibility;
