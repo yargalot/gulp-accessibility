4f90fd11e0cf1f2b5e86b0df83d1eb924df350b1
Steven John Miller <steven.jmiller@gmail.com>
2015-04-20 18:46:37 +1000

    Init jshint

---
+++ libs/accessSniff.js
@@ -0,0 +1,27 @@
+//
+// Grunt AccessSniff Task
+// ------------------------
+
+module.exports = function(grunt) {
+  
+  var accessSniff = require('access-sniff');
+
+  grunt.registerMultiTask('accessibility', 'Use HTML codesniffer to grade accessibility', function() {
+
+    var done = this.async();
+    var options = this.options({
+      ignore: [],
+      verbose: true,
+      force: false,
+      domElement: true,
+      reportType: null,
+      reportLocation : 'reports',
+      accessibilityrc: false,
+      accessibilityLevel: 'WCAG2A',
+    });
+
+    accessSniff.start(this.filesSrc, options, done);
+
+  });
+
+};
