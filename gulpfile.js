e8eebcb879f666166e6e9faf05737f574782b005
Steven John Miller <steven.jmiller@gmail.com>
2015-04-20 22:20:09 +1000

    Get npm test working

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,14 @@ var stylish = require('jshint-stylish');
 var access = require('./libs/accessSniff.js');
 
 gulp.task('lint', function() {
-  return gulp.src('./libs/*.js')
+  return gulp.src(['./libs/*.js', 'gulpfile.js'])
     .pipe(jshint())
     .pipe(jshint.reporter(stylish));
 });
 
-gulp.task('test', function() {
+gulp.task('accessSniff', function() {
   return gulp.src('./example/**/*.html')
     .pipe(access());
 });
+
+gulp.task('test', ['lint', 'accessSniff']);
